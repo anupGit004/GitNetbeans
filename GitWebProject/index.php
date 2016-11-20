db1e39ceb3e8e6e4186570c23c9fe0e77aa82247
anupGit004 <anup.gupta@hytechpro.com>
2016-11-20 12:29:07 +0530

    updated by remote

--- GitWebProject/index.php
+++ GitWebProject/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Index File</title>
+        <title>Home Page</title>
     </head>
     <body>
         <?php
