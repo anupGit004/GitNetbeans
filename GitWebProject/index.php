e929b039274f97f9618b61adca20b753805b7622
anupgit004 <anup.gupta@hytechpro.com>
2016-11-20 12:07:08 +0530

    comment by local

--- GitWebProject/index.php
+++ GitWebProject/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "hello";
+        echo "hello"; // new changes by local 
         ?>
     </body>
 </html>
